5cf5f79f953a3a2ec417ef82d37ea18817083f05
TheKid77 <andymcc77@blueyonder.co.uk>
2021-07-21 15:42:24 +0100

    Heroku

---
+++ Includes/Footer.php
@@ -0,0 +1,13 @@
+    <!-- FOOTER -->
+    <footer class="bg-dark text-white">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+          <p class="lead text-center">Theme By | Andy McConville | <span id="year"></span> &copy; ----All right Reserved.</p>
+          <p class="text-center small"><a style="color: white; text-decoration: none; cursor: pointer;" href="#" target="_blank"> This site is only used for Study purpose andymc.com have all the rights. no one is allow to distribute copies other then <br>&trade; andymc.com</a></p>
+           </div>
+         </div>
+      </div>
+    </footer>
+    <div style="height:10px; background:#27aae1;"></div>
+    <!-- FOOTER END-->
--- Login.php
+++ Login.php
@@ -50,7 +50,7 @@ if (isset($_POST["Submit"])) {
   <div style="height:10px; background:#27aae1;"></div>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-      <a href="#" class="navbar-brand"> ANDYMC.COM</a>
+      <a href="index.php" class="navbar-brand"> ANDYMC.COM</a>
       <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarcollapseCMS">
         <span class="navbar-toggler-icon"></span>
       </button>
