9efbbffbe64fc36e6538fcbfd490c32ca7924e62
TheKid77 <andymcc77@blueyonder.co.uk>
2021-07-22 20:06:10 +0100

    correct typo

--- Includes/Footer.php
+++ Includes/Footer.php
@@ -4,7 +4,7 @@
         <div class="row">
           <div class="col">
           <p class="lead text-center">Theme By | Andy McConville | <span id="year"></span> &copy; ----All right Reserved.</p>
-          <p class="text-center small"><a style="color: white; text-decoration: none; cursor: pointer;" href="#" target="_blank"> This site is only used for Study purpose andymc.com have all the rights. no one is allow to distribute copies other then <br>&trade; andymc.com</a></p>
+          <p class="text-center small"><a style="color: white; text-decoration: none; cursor: pointer;" href="#" target="_blank"> This site is only used for Study purpose andymc.com have all the rights. <br>&trade; andymc.com</a></p>
            </div>
          </div>
       </div>
